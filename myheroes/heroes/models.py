5cfc39706a10b53541a2cf9af42f1ebd92de3f12
brian-burdine <brian.burdine@student.awesomeinc.org>
2023-03-30 15:13:52 +0000

    Made a heroes project

--- myheroes/heroes/models.py
+++ myheroes/heroes/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Ability(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "abilities"
+
+class Relationship(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+        
 class Hero(models.Model):
     name = models.CharField(max_length=100)
     about_me = models.CharField(max_length=200)
     biography = models.TextField()
+    abilities = models.ManyToManyField(Ability)
 
     class Meta:
         ordering = ["name"]
         verbose_name_plural = "heroes"
-
-class AbilityType(models.Model):
-    name = models.CharField(max_length=80)
-
-class RelationshipType(models.Model):
-    name = models.CharField(max_length=80)
+    
+    def __str__(self):
+        return self.name
